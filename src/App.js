939439a463c413e8584f0e54fa7626a5cf7170b9
Joao Moreira <jgmoreira1809@gmail.com>
2022-06-24 13:24:50 -0700

    Lint Requisito 1 novamente

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ class App extends React.Component {
     return (
       <div>
         <h1>Tryunfo!</h1>
-        <Form />
+        <Form
+        cardName=""
+        cardDescription=""
+
+        />
       </div>
     );
   }
