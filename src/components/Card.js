407f6fd72932227b7637b252d06765cc93ae9ea1
Joao Moreira <jgmoreira1809@gmail.com>
2022-06-24 14:22:35 -0700

    Requisito 3

---
+++ src/components/Card.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const isTrunfoCard = (cardInfo) => {
+      if (cardInfo) {
+        return (<span data-testid="trunfo-card">Super Trunfo</span>);
+      }
+    };
+
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
+    return (
+      <div>
+        <h3 data-testid="name-card">{ cardName }</h3>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <span data-testid="attr1-card">{ cardAttr1 }</span>
+        <span data-testid="attr2-card">{ cardAttr2 }</span>
+        <span data-testid="attr3-card">{ cardAttr3 }</span>
+        <span data-testid="rare-card">{ cardRare }</span>
+        <span>{ isTrunfoCard(cardTrunfo) }</span>
+      </div>
+
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Form.css';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
@@ -15,7 +16,7 @@ class Form extends React.Component {
       hasTrunfo,
       isSaveButtonDisabled,
       onInputChange,
-      onSaveButtonClick
+      onSaveButtonClick,
     } = this.props;
 
     return (
@@ -159,5 +160,20 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.string.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
