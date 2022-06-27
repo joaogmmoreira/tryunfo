2ca8e9e983b89ff32453ed104538d4f46e075799
Joao Moreira <jgmoreira1809@gmail.com>
2022-06-27 12:30:12 -0700

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -9,16 +9,74 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
-      onSaveButtonClick: () => { },
     };
+  }
+
+  checkInput = () => {
+    const { cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+    } = this.state;
+
+    if (cardName.length === 0
+      || cardDescription.length === 0
+      || cardImage.length === 0
+      || cardRare.length === 0
+    ) {
+      // return console.log('Zuou');
+      return true;
+    }
+    // return console.log('Tá na moral');
+    return false;
+  }
+
+  checkAttrBetween0and90 = () => {
+    const minAttr = 0;
+    const maxAttr = 90;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+
+    if ((Number(cardAttr1) > maxAttr || Number(cardAttr1) < minAttr)
+      || (Number(cardAttr2) > maxAttr || Number(cardAttr2) < minAttr)
+      || (Number(cardAttr3) > maxAttr || Number(cardAttr3) < minAttr)) {
+      // console.log('zuou');
+      return true;
+    }
+    // console.log('tá na moral');
+    return false;
+  }
+
+  checkAttrSum = () => {
+    const maxSum = 210;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    if (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) > maxSum) {
+      // return console.log('zuou');
+      return true;
+    }
+    // return console.log('Tá na moral');
+    return false;
+  }
+
+  handleButtonChange = (func1, func2, func3) => {
+    if (func1 || func2 || func3) {
+      // console.log('True');
+      this.setState({
+        isSaveButtonDisabled: true,
+      }, () => {});
+    } else {
+      // console.log('false');
+      this.setState({
+        isSaveButtonDisabled: false,
+      }, () => {});
+    }
   }
 
   onInputChange = ({ target }) => {
@@ -26,14 +84,22 @@ class App extends React.Component {
     const value = type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => {
+      this.handleButtonChange(this.checkInput(),
+        this.checkAttrBetween0and90(), this.checkAttrSum());
     });
   }
+
+  // onSaveButtonClick: () => { },
 
   render() {
     return (
       <div>
         <h1>Tryunfo!</h1>
-        <Form { ...this.state } onInputChange={ this.onInputChange } />
+        <Form
+          { ...this.state }
+          onInputChange={ this.onInputChange }
+        />
         <Card { ...this.state } />
       </div>
     );
